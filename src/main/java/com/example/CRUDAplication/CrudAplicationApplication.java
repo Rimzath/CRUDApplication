3d11575fc32a9797a000f4e7435e4b4bf89a243e
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2024-11-16 14:16:35 +0530

    Initial project

---
+++ src/main/java/com/example/CRUDAplication/CrudAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.CRUDAplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CRUDAplication/CrudAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRUDAplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
